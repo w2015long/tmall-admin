548b7bd2e9c2ff530f5daadcb1644b038e571f25
Jsen <w2015long@qq.com>
2019-04-17 17:36:29 +0800

    add home antd

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,6 @@
 
 import React,{Component} from 'react';
+import {Card} from 'antd'
 import {getUserName} from 'util'
 import AdminLayout from 'common/layout'
 import './index.css';
@@ -7,9 +8,28 @@ import './index.css';
 class Home extends Component{
 	render(){
 		return (
-			<AdminLayout>
-				welcome index
-			</AdminLayout>
+			<div className="Home">
+				<AdminLayout>
+				    <Card
+				      title="用户数量"
+				      hoverable={true}
+				    >
+				      <p>100</p>
+				    </Card>	
+				    <Card
+				      title="商品数量"
+				      hoverable={true}
+				    >
+				      <p>101</p>
+				    </Card>	
+				    <Card
+				      title="订单数量"
+				      hoverable={true}
+				    >
+				      <p>1019</p>
+				    </Card>			    			    		
+				</AdminLayout>
+			</div>	
 		)
 	}
 }
